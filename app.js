7a44a8e14ed41606d30279d5c76eb4894b7f0dc5
Varunkumar <hrvarunkumar2000@gmail.com>
2025-07-29 17:33:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const requestHandler = (req, res) => {
-  res.end('Hello from Riyaz Todlya');
+  res.end('Hello from Riyaz Todlya Hombulli');
 };
 
 const server = http.createServer(requestHandler);
